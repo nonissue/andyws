817ca09bc55f662e6dc293f6db673d1b983f2858
nonissue <andy@nonissue.org>
2021-02-09 18:17:03 -0700

    style: add logo, use ProjectCard comp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,216 +1,60 @@
+import { ProjectCard, Timeline } from "src/components";
+
 const Index: React.FunctionComponent = () => {
   return (
-    <section className="text-gray-400 bg-gray-900 body-font">
-      {/* <h1 className="text-4xl text-gray-200">NON</h1> */}
-      <div className="container px-5 py-24 mx-auto">
-        <h1 className="text-6xl mx-6 text-lb-300 inline-block py-1 px-3 my-10 font-bold">
-          NON
-        </h1>
-        <div className="flex flex-wrap -m-4">
-          <div className="p-4 md:w-1/3">
-            <div className="h-full border-2 border-gray-800 rounded-lg overflow-hidden">
-              <img
-                className="lg:h-48 md:h-36 w-full object-cover object-center"
-                src="https://dummyimage.com/720x400"
-                alt="blog"
-              />
-              <div className="p-6">
-                <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
-                  CATEGORY
-                </h2>
-                <h1 className="title-font text-lg font-medium text-white mb-3">
-                  The Catalyzer
-                </h1>
-                <p className="leading-relaxed mb-3">
-                  Photo booth fam kinfolk cold-pressed sriracha leggings
-                  jianbing microdosing tousled waistcoat.
-                </p>
-                <div className="flex items-center flex-wrap ">
-                  <a className="text-yellow-300 inline-flex items-center md:mb-2 lg:mb-0">
-                    Learn More
-                    <svg
-                      className="w-4 h-4 ml-2"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path d="M5 12h14"></path>
-                      <path d="M12 5l7 7-7 7"></path>
-                    </svg>
-                  </a>
-                  <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-800">
-                    <svg
-                      className="w-4 h-4 mr-1"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                      <circle cx="12" cy="12" r="3"></circle>
-                    </svg>
-                    1.2K
-                  </span>
-                  <span className="text-gray-500 inline-flex items-center leading-none text-sm">
-                    <svg
-                      className="w-4 h-4 mr-1"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                    </svg>
-                    6
-                  </span>
-                </div>
-              </div>
-            </div>
+    <>
+      <section className="text-gray-400 bg-gray-900 body-font">
+        <div className="container px-5 py-6 mx-auto lg:px-0">
+          <div className="w-full flex justify-start lg:justify-start  mb-4">
+            <img
+              src="/logo-lightfg-crop.png"
+              alt="nonissue logo"
+              className="object-contain h-full max-h-12 p-1 hover:opacity-10 cursor-pointer transition-opacity duration-500"
+            />
           </div>
-          <div className="p-4 md:w-1/3">
-            <div className="h-full border-2 border-gray-800 rounded-lg overflow-hidden">
-              <img
-                className="lg:h-48 md:h-36 w-full object-cover object-center"
-                src="https://dummyimage.com/721x401"
-                alt="blog"
-              />
-              <div className="p-6">
-                <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
-                  CATEGORY
-                </h2>
-                <h1 className="title-font text-lg font-medium text-white mb-3">
-                  The 400 Blows
-                </h1>
-                <p className="leading-relaxed mb-3">
-                  Photo booth fam kinfolk cold-pressed sriracha leggings
-                  jianbing microdosing tousled waistcoat.
-                </p>
-                <div className="flex items-center flex-wrap">
-                  <a className="text-yellow-300 inline-flex items-center md:mb-2 lg:mb-0">
-                    Learn More
-                    <svg
-                      className="w-4 h-4 ml-2"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path d="M5 12h14"></path>
-                      <path d="M12 5l7 7-7 7"></path>
-                    </svg>
-                  </a>
-                  <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-800">
-                    <svg
-                      className="w-4 h-4 mr-1"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                      <circle cx="12" cy="12" r="3"></circle>
-                    </svg>
-                    1.2K
-                  </span>
-                  <span className="text-gray-500 inline-flex items-center leading-none text-sm">
-                    <svg
-                      className="w-4 h-4 mr-1"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                    </svg>
-                    6
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="p-4 md:w-1/3">
-            <div className="h-full border-2 border-gray-800 rounded-lg overflow-hidden">
-              <img
-                className="lg:h-48 md:h-36 w-full object-cover object-center"
-                src="https://dummyimage.com/722x402"
-                alt="blog"
-              />
-              <div className="p-6">
-                <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
-                  CATEGORY
-                </h2>
-                <h1 className="title-font text-lg font-medium text-white mb-3">
-                  Shooting Stars
-                </h1>
-                <p className="leading-relaxed mb-3">
-                  Photo booth fam kinfolk cold-pressed sriracha leggings
-                  jianbing microdosing tousled waistcoat.
-                </p>
-                <div className="flex items-center flex-wrap ">
-                  <a className="text-yellow-300 inline-flex items-center md:mb-2 lg:mb-0">
-                    Learn More
-                    <svg
-                      className="w-4 h-4 ml-2"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path d="M5 12h14"></path>
-                      <path d="M12 5l7 7-7 7"></path>
-                    </svg>
-                  </a>
-                  <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-800">
-                    <svg
-                      className="w-4 h-4 mr-1"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                      <circle cx="12" cy="12" r="3"></circle>
-                    </svg>
-                    1.2K
-                  </span>
-                  <span className="text-gray-500 inline-flex items-center leading-none text-sm">
-                    <svg
-                      className="w-4 h-4 mr-1"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                    </svg>
-                    6
-                  </span>
-                </div>
-              </div>
-            </div>
+
+          <div className="flex flex-wrap -m-4">
+            <ProjectCard
+              title="Test Project"
+              description="This is just a test project. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+              type="web"
+              stack={["react", "prisma", "postgres", "typescript"]}
+              link="/projects/test"
+              imgsrc="https://picsum.photos/seed/blotestws/640/360"
+            />
+            <ProjectCard
+              title="The Catalyzer"
+              description="Photo booth fam kinfolk cold-pressed sriracha leggings
+                    jianbing microdosing tousled waistcoat."
+              type="NODE.JS"
+              stack={["react", "next.js"]}
+              link="/projects/thecatalyzer"
+              imgsrc="https://picsum.photos/seed/cata/640/360"
+            />
+            <ProjectCard
+              title="The 400 Blows"
+              description="Photo booth fam kinfolk cold-pressed sriracha leggings
+                    jianbing microdosing tousled waistcoat."
+              type="NODE.JS"
+              stack={["react", "next.js"]}
+              link="/projects/400blows"
+              imgsrc="https://picsum.photos/seed/blows/640/360"
+            />
+            <ProjectCard
+              title="Shooting Stars"
+              description="Photo booth fam kinfolk cold-pressed sriracha leggings
+                    jianbing microdosing tousled waistcoat."
+              type="NODE.JS"
+              stack={["react", "prisma", "postgres", "typescript"]}
+              link="/projects/shootingstars"
+              imgsrc="https://picsum.photos/seed/shooting/640/360"
+            />
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+
+      <Timeline />
+    </>
   );
 };
 
