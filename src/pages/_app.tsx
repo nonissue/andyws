06ef90e548309b5bea45aece1649e33f3abe0533
nonissue <andy@nonissue.org>
2021-04-27 17:31:22 -0600

    feat: add `inspct`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import type { NextComponentType, NextPageContext } from "next";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { ThemeProvider } from "next-themes";
+import Inspect from "inspx";
 import { SiteContextProvider } from "src/lib/context";
 import { Nav } from "src/components";
 
@@ -29,16 +30,21 @@ const MyApp: React.FunctionComponent<AppPropsWithLayout> = ({
   const getLayout = Component.getLayout || ((page) => page);
 
   return (
-    <ThemeProvider forcedTheme={Component.theme || undefined} attribute="class">
-      <SiteContextProvider>
-        <Head>
-          <title>non</title>
-        </Head>
+    <Inspect>
+      <ThemeProvider
+        forcedTheme={Component.theme || undefined}
+        attribute="class"
+      >
+        <SiteContextProvider>
+          <Head>
+            <title>non</title>
+          </Head>
 
-        <Nav />
-        {getLayout(<Component {...pageProps} />, pageProps)}
-      </SiteContextProvider>
-    </ThemeProvider>
+          <Nav />
+          {getLayout(<Component {...pageProps} />, pageProps)}
+        </SiteContextProvider>
+      </ThemeProvider>
+    </Inspect>
   );
 };
 
