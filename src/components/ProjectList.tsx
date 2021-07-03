79b6be87c3f64591fb049e8985f7aa618c65f662
Andy Williams <andy@nonissue.org>
2021-07-03 16:01:15 -0600

    cleanup: remove old projectitem, projectlist

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -6,7 +6,7 @@ const ProjectList = () => {
     <div className="bg-white dark:bg-almostblack">
       <div className="grid grid-cols-1 md:grid-cols-3 items-center mb-4">
         <h2
-          className="hidden max-w-md mx-auto text-xl font-bold w-full text-gray-700 text-left 
+          className=" max-w-md mx-auto text-xl font-bold w-full text-gray-700 text-left 
            dark:text-gray-100
             md:mt-0 md:max-w-none md:col-span-3 md:mx-0"
         >
