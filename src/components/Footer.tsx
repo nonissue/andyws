594d55ae77b65a90d02d2d851066b8080a2909c8
Andy Williams <andy@nonissue.org>
2021-07-21 00:22:11 -0600

    bump

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="  z-0 static flex  justify-start px-6 md:px-8 max-w-3xl mx-auto mb-8 ">
+    <div className="z-0 static flex justify-start px-6 md:px-8 max-w-3xl mx-auto mb-8 ">
       <div className="font-display capitalize  font-bold leading-relaxed  text-md sm:text-md md:text-center text-center border-0 border-gray-300 bg-opacity-100 text-almostblack dark:text-gray-50 dark:border-gray-600 dark:bg-opacity-90 opacity-60">
         <span className="font-mono lowercase text-sm font-normal italic opacity-100">
           BY
