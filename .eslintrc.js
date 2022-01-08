dcedb28ac11895f5f886a1b441adbae82c500327
Andy Williams <andy@nonissue.org>
2022-01-07 17:26:45 -0700

    misc: add eslint next passHref rule, react hooks, exhaustive deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,39 +5,42 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser", // the TypeScript parser we installed earlier
+  parser: '@typescript-eslint/parser', // the TypeScript parser we installed earlier
   parserOptions: {
     ecmaFeatures: { jsx: true }, // Allows for the parsing of JSX
   },
-  plugins: ["@typescript-eslint", "import", "react"],
+  plugins: ['@typescript-eslint', 'import', 'react', 'react-hooks'],
   extends: [
-    "eslint:recommended", // eslint default rules
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "plugin:@typescript-eslint/eslint-recommended", // eslint TypeScript rules (github.com/typescript-eslint/typescript-eslint)
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended", // eslint react rules (github.com/yannickcr/eslint-plugin-react)
-    "plugin:jsx-a11y/recommended", // accessibility plugin
-    "plugin:@next/next/recommended",
+    'eslint:recommended', // eslint default rules
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended', // eslint TypeScript rules (github.com/typescript-eslint/typescript-eslint)
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended', // eslint react rules (github.com/yannickcr/eslint-plugin-react)
+    'plugin:jsx-a11y/recommended', // accessibility plugin
+    'plugin:react-hooks/recommended',
+    'plugin:@next/next/recommended',
   ],
   rules: {
-    "react/prop-types": "off", // We turn off prop-types rule, as we will use TypeScript's types instead.
-    "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "jsx-a11y/anchor-is-valid": "off",
-    "@typescript-eslint/no-unused-vars": ["error"],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'jsx-a11y/anchor-is-valid': 'off',
+    'react/prop-types': 'off', // We turn off prop-types rule, as we will use TypeScript's types instead.
+    'react/react-in-jsx-scope': 'off',
+    '@next/next/link-passhref': ['error'],
+    'react-hooks/exhaustive-deps': 'warn',
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
-        project: ".",
+        project: '.',
       },
     },
     react: {
-      version: "detect",
+      version: 'detect',
     },
-    files: ["**/*.ts", "**/*.tsx"],
+    files: ['**/*.ts', '**/*.tsx'],
   },
 };
