b7eb590119a367d4f4aa0ead077ceb0c252609c7
Andy Williams <andy@nonissue.org>
2022-01-10 19:44:10 -0700

    style: fix header dark link color

--- src/components/NewHeader/HeaderLink.tsx
+++ src/components/NewHeader/HeaderLink.tsx
@@ -9,7 +9,7 @@ const NewHeaderLink = ({ title, href }: NewHeaderLinkProps) => {
   return (
     <div>
       <Link href={href}>
-        <a className="font-sans text-xs font-medium tracking-wide hover:text-blue-600 dark:hover:text-blue-400 capitalize opacity-90 transition-colors duration-200 sm:text-base">
+        <a className="font-sans text-xs font-medium tracking-wide text-slate-700 hover:text-blue-600 dark:text-slate-300 dark:hover:text-blue-400 capitalize opacity-90 transition-colors duration-200 sm:text-base">
           {title}
         </a>
       </Link>
