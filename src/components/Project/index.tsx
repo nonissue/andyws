7a76b08286206685552d40a92d58aed2359ce8a1
nonissue <andy@nonissue.org>
2025-12-23 22:30:48 -0700

    delete unused project code (components/pages)

--- src/components/Project/index.tsx
+++
@@ -1,4 +0,0 @@
-export { ProjectItem } from "./ProjectItem";
-export { ProjectItemHeader } from "./ProjectItemHeader";
-export { ProjectList } from "./ProjectList";
-export { ProjectTag } from "./ProjectTag";
